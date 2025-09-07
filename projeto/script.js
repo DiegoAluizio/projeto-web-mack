bb1d973b263005150286fde53e11b58da2fb960e
Jonatas-G-Oliveira <jonatas11oliveira@live.com>
2025-09-07 10:47:31 -0300

    Feat:Versão da página mockada para web

--- projeto/script.js
+++ projeto/script.js
@@ -1,41 +1,41 @@
-let pessoa = {
-    nome: "ANa",
-    idade: 25,
-    profissao: "Dev"
+// let pessoa = {
+//     nome: "ANa",
+//     idade: 25,
+//     profissao: "Dev"
 
-}
+// }
 
-let usuario = {
-    nome: "Carlos",
-    saudacao: function(){
-        return `Ola, me nome é ${this.nome}!`
-    }
-}
-//Duas formas de chamar
-console.log(pessoa.nome)
-console.log(pessoa['idade'])
-console.log(pessoa.saudacao())
+// let usuario = {
+//     nome: "Carlos",
+//     saudacao: function(){
+//         return `Ola, me nome é ${this.nome}!`
+//     }
+// }
+// //Duas formas de chamar
+// console.log(pessoa.nome)
+// console.log(pessoa['idade'])
+// console.log(pessoa.saudacao())
 
-//Deleta a informação
-delete pessoa.profissao
+// //Deleta a informação
+// delete pessoa.profissao
 
-for (let chave in pessoa){
-    console.log(chave, pessoa[chave]);
-}
+// for (let chave in pessoa){
+//     console.log(chave, pessoa[chave]);
+// }
 
-//Object.keys(Obbjec).values()
-console.log(Object.keys(pessoa))
-console.log(Object.values(pessoa))
+// //Object.keys(Obbjec).values()
+// console.log(Object.keys(pessoa))
+// console.log(Object.values(pessoa))
 
-//JSON - 
-{
-    "nome": "Lucas",
-    "idade" : 30,
-    "habilidades": ["JavaScript","React","Node"]  
-}
-//DO objeto para Json
-let jsonDados = JSON.stringify(dados);
+// //JSON - 
+// {
+//     "nome": "Lucas",
+//     "idade" : 30,
+//     "habilidades": ["JavaScript","React","Node"]  
+// }
+// //DO objeto para Json
+// let jsonDados = JSON.stringify(dados);
 
-//Do json para Objeto
-let objeto = JSON.parse(textoJson)
-console.log(jsonDados);
+// //Do json para Objeto
+// let objeto = JSON.parse(textoJson)
+// console.log(jsonDados);
