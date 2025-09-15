18f845876958ec7252c29df515f4ee04ac0273a3
unknown <jonatas11oliveira@live.com>
2025-09-15 10:29:55 -0300

    Iniciano versão JavasScript

--- projeto/script.js
+++ projeto/script.js
@@ -1,27 +1,63 @@
-// let pessoa = {
-//     nome: "ANa",
-//     idade: 25,
-//     profissao: "Dev"
+let card = {
+    titulo: 'Crochet Bonito',
+    img: 'assets/img_placeholder.png',
+    alt: 'imagem',
+    valor: 220
+}
 
-// }
+let comentarios = {
+    titulo: 'Muito fera',
+    usuario: 'Diegão',
+    comentario: "Lih's Crochet é muito bom eu amei muito tudooo!!"
+}
 
-// let usuario = {
-//     nome: "Carlos",
-//     saudacao: function(){
-//         return `Ola, me nome é ${this.nome}!`
-//     }
-// }
-// //Duas formas de chamar
-// console.log(pessoa.nome)
-// console.log(pessoa['idade'])
-// console.log(pessoa.saudacao())
 
-// //Deleta a informação
-// delete pessoa.profissao
+//-- Como Fazer Aplicação Animada com HTML, Javascript e CSS [Passo a Passo]
+//-- Crazy Scroll Animation Using CSS Only
 
-// for (let chave in pessoa){
-//     console.log(chave, pessoa[chave]);
-// }
+
+// ------ Criação dos componentes de Produto
+const carrossel_produto = document.querySelector("#carrossel_produtos")
+for(let i = 0; i < 4; i++){
+    const card_produto = document.createElement('div')
+    card_produto.className = "card_produto"
+    card_produto.innerHTML = `
+    <img src = ${card.img} alt = '${card.alt}'>
+    <h2>${card.titulo} ${i+1}</h2>
+    <p>De R$${card.valor}</p>
+    `
+    carrossel_produto.append(card_produto)
+}
+
+// ---- Criacão dos componentes de Destaque
+const carrossel_destaque = document.querySelector("#carrossel_destaque")
+for(let i = 0;i < 2; i++){
+    const card_destaque = document.createElement('div')
+    card_destaque.className  = "card_destaque"
+    card_destaque.innerHTML = 
+    `
+        <img src = ${card.img} alt = '${card.alt}'>
+        <h2>${card.titulo} ${i+1}</h2>
+        <p>De R$${card.valor}</p>
+    `    
+    carrossel_destaque.append(card_destaque)
+}
+
+
+// ---- Criacão dos componentes de Comentarios
+const carrossel_comentario = document.querySelector("#carrossel_comentarios")
+for(let i = 0;i < 3; i++){
+    const card_comentario = document.createElement('div')
+    card_comentario.className = 'card_comentario'
+    card_comentario.innerHTML = `
+        <h2>${comentarios.titulo} ${i+1}</h2>
+        <p>${comentarios.comentario}</p>
+        <h3>${comentarios.usuario}</h3>   
+    `
+    carrossel_comentario.append(card_comentario)
+}
+
+
 
 // //Object.keys(Obbjec).values()
 // console.log(Object.keys(pessoa))
